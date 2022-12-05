9cfeb673808072977b1231057282b350064a67d7
hiren patel <hirenpatel0526@yahoo.com>
2022-12-05 14:16:33 -0800

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <Footer className='Footer-bg-light fixed-bottom'>
+      <a 
+            href='https://github.com/radhashyamsundardas'> 
+            <img src='https://www.oomnitza.com/wp-content/uploads/2022/06/github-logo.png' alt='Github' style={{height:100}}/>
+            </a>
+
+            <a 
+            href='https://www.linkedin.com/in/radhashyamsundar-das-036a2b251/'> 
+            <img src='https://www.clipartmax.com/png/middle/304-3041366_linkedin-logotype-button-free-icon-linkedin-logo-b-w.png' alt='Github' style={{height:100}} />
+            </a>
+
+            <a 
+            href='https://lamarionsbazaar.com/'> 
+            <img src='https://cdn.shopify.com/s/files/1/0270/2003/4146/files/LB-Logo-sq_5b092138-591f-4dcb-807e-83ab22be8433_720x.jpg?v=1641595689' alt='Lamarions Bazaar' style={{height:100}} />
+            </a>
+            <br></br>
+            <br></br>
+
+            <div>
+                <img src='https://storage.googleapis.com/wzukusers/user-18209628/images/5c083c846ba38dG1mXw6/namaste-hand-drawn-vector-20399758.jpg' alt='Namaste' style={{height:75}}/>
+            <h3>from Hiren Patel</h3>
+            </div>
+            
+    </Footer>
   )
 }
 
