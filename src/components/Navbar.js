9f12617d7a0c38fe6b5bd38352e74ab4bd87f89e
hiren patel <hirenpatel0526@yahoo.com>
2022-12-05 13:09:53 -0800

    finished navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+       <h1> HIREN PATEL'S PORTFOLIO</h1>
+       <ul class="Navbar-nav ml-auto navitem-indent">
+       <li class="nav-item">
+                    <Link to="/Home">
+                        <div >
+                            <img src='https://www.clipartmax.com/png/small/14-142308_home-icon-png-orange.png' height={30}></img>
+
+                        </div>
+                    </Link>
+                </li>
+            <li class="nav-item">
+                    <Link to="/Aboutme">
+                        <div >
+                            <h2>About Me</h2>
+                        </div>
+                    </Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/Portfolio">
+                        <div >
+                            <h2>Portfolio</h2>
+                        </div>
+                    </Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/Contactme">
+                        <div >
+                            <h2>Contact Me</h2>
+                        </div>
+                    </Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/Resume">
+                        <div >
+                            <h2>Resume</h2>
+                        </div>
+                    </Link>
+                </li>
+       </ul>
+    </div>
+  )
+}
+       
+      
+
+export default Navbar
