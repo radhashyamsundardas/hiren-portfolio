2d46a9b847fa52b0346c32982b854af8c76f17f6
hiren patel <hirenpatel0526@yahoo.com>
2022-12-05 16:42:17 -0800

    finished with home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,59 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div id='symbols'>
+      <h2>Technical Skills</h2>
+      <img src='https://img.shields.io/badge/HTML5-E34F26.svg?style=for-the-badge&logo=HTML5&logoColor=white' alt='Html'></img>
+      <img src='https://img.shields.io/badge/CSS3-1572B6.svg?style=for-the-badge&logo=CSS3&logoColor=white' alt='Css'></img>
+      <img src='https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black' alt='Javascript'></img>
+      <img src='https://img.shields.io/badge/Git-F05032.svg?style=for-the-badge&logo=Git&logoColor=white' alt='Git'></img>
+      <img src='https://img.shields.io/badge/GitHub-181717.svg?style=for-the-badge&logo=GitHub&logoColor=white' alt='Github'></img>
+      <img src='https://img.shields.io/badge/GitHub%20Pages-222222.svg?style=for-the-badge&logo=GitHub-Pages&logoColor=white' alt='Github Pages'></img>
+      <img src='https://img.shields.io/badge/GitLab-FC6D26.svg?style=for-the-badge&logo=GitLab&logoColor=white' alt='Gitlab'></img>
+      <br></br>
+      <img src='https://img.shields.io/badge/Bootstrap-7952B3.svg?style=for-the-badge&logo=Bootstrap&logoColor=white' alt='Bootstrap'></img>
+      <img src='https://img.shields.io/badge/jQuery-0769AD.svg?style=for-the-badge&logo=jQuery&logoColor=white' alt='Jquery'></img>
+      <img src='https://img.shields.io/badge/Node.js-339933.svg?style=for-the-badge&logo=nodedotjs&logoColor=white' alt='Nodejs'></img>
+      <img src='https://img.shields.io/badge/Nodemon-76D04B.svg?style=for-the-badge&logo=Nodemon&logoColor=white' alt='Nodemon'></img>
+      <img src='https://img.shields.io/badge/MongoDB-47A248.svg?style=for-the-badge&logo=MongoDB&logoColor=white' alt='Mongodb'></img>
+      <img src='https://img.shields.io/badge/Sequelize-52B0E7.svg?style=for-the-badge&logo=Sequelize&logoColor=white' alt='Sequelize'></img>
+      <img src='https://img.shields.io/badge/PWA-5A0FC8.svg?style=for-the-badge&logo=PWA&logoColor=white' alt='Progrssive Web Application'></img>
+      <br></br>
+      <img src='https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=React&logoColor=black' alt='React'></img>
+      <img src='https://img.shields.io/badge/React%20Query-FF4154.svg?style=for-the-badge&logo=React-Query&logoColor=white' alt='React Query'></img>
+      <img src='https://img.shields.io/badge/React%20Router-CA4245.svg?style=for-the-badge&logo=React-Router&logoColor=white' alt='React Router'></img>
+      <img src='https://img.shields.io/badge/React%20Table-FF4154.svg?style=for-the-badge&logo=React-Table&logoColor=white' alt='React Table'></img>
+      <img src='https://img.shields.io/badge/Create%20React%20App-09D3AC.svg?style=for-the-badge&logo=Create-React-App&logoColor=white' alt='Create react app'></img>
+      <img src='https://img.shields.io/badge/React%20Hook%20Form-EC5990.svg?style=for-the-badge&logo=React-Hook-Form&logoColor=white' alt='React Hook Form'></img>
+
+      <br></br>
+      <img src='https://img.shields.io/badge/MySQL-4479A1.svg?style=for-the-badge&logo=MySQL&logoColor=white' alt='Mysql'></img>
+      <img src='https://img.shields.io/badge/GraphQL-E10098.svg?style=for-the-badge&logo=GraphQL&logoColor=white' alt='Graphql'></img>
+      <img src='https://img.shields.io/badge/Apollo%20GraphQL-311C87.svg?style=for-the-badge&logo=Apollo-GraphQL&logoColor=white' alt='Apollo Graphql'></img>
+      <img src='https://img.shields.io/badge/Sass-CC6699.svg?style=for-the-badge&logo=Sass&logoColor=white' alt='Sass'></img>
+      <img src='https://img.shields.io/badge/JSON%20Web%20Tokens-000000.svg?style=for-the-badge&logo=JSON-Web-Tokens&logoColor=white' alt='Json Web Tokens'></img>
+      <img src='https://img.shields.io/badge/Stripe-008CDD.svg?style=for-the-badge&logo=Stripe&logoColor=white' alt='Stripe'></img>
+      <img src='https://img.shields.io/badge/Figma-F24E1E.svg?style=for-the-badge&logo=Figma&logoColor=white' alt='Figma'></img>
+      <br></br>
+      <img src='https://img.shields.io/badge/Heroku-430098.svg?style=for-the-badge&logo=Heroku&logoColor=white' alt='Heroku'></img>
+      <img src='https://img.shields.io/badge/Netlify-00C7B7.svg?style=for-the-badge&logo=Netlify&logoColor=white' alt='Netlify'></img>
+      <img src='https://img.shields.io/badge/.ENV-ECD53F.svg?style=for-the-badge&logo=dotenv&logoColor=black' alt='.env'></img>
+      <img src='https://img.shields.io/badge/Screencastify-FF8282.svg?style=for-the-badge&logo=Screencastify&logoColor=white' alt='.screencastify'></img>
+      <img src='https://img.shields.io/badge/Slack-4A154B.svg?style=for-the-badge&logo=Slack&logoColor=white' alt='.Slack'></img>
+      <img src='https://img.shields.io/badge/Insomnia-4000BF.svg?style=for-the-badge&logo=Insomnia&logoColor=white' alt='Insomnia'></img>
+      <img src='https://img.shields.io/badge/Postman-FF6C37.svg?style=for-the-badge&logo=Postman&logoColor=white' alt='Postman'></img>
+
+    </div>
   )
 }
+
+
+
+
+
+
+
+
+
 
 export default Home
